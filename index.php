d2c8b87b06c3d0a781939863a05879353488796f
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-02-18 16:36:47 +0000

    done 1

--- index.php
+++ index.php
@@ -295,3 +295,479 @@
 
 </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // この下に連想配列を作ってください
+    $scores = array(
+      "数学" => 70 ,
+      
+      "英語" => 90 ,
+      
+      "国語" => 80 
+      
+      );
+    
+    $scores["国語"] += 5 ;
+    echo $scores["国語"];
+  ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // この下にfor文を書いてください
+    for ($i = 51 ; $i <= 100 ; $i++ ){
+      echo $i;
+      echo "<br>";
+    }
+    
+  ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 変数$iを定義してください
+    $i = 2;
+    while($i <= 100){
+      echo $i;
+      $i = $i + 2 ;
+      echo "<br>";
+    }
+    
+    // while文を書いてください
+    
+    
+  ?>
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // この下にfor文を書いてください
+    for($i = 1 ; $i <= 1000; $i++) {
+      if($i > 500){
+        break;
+      }
+      echo $i ;
+      echo "<br>";
+    }
+    
+  ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // この下にfor文を書いてください
+    for($i=1;$i<=1000;$i++){
+      if($i%3==0){
+        continue;
+      }
+      echo $i;
+      echo "<br>";
+    }
+    
+  ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    $scores = array('数学' => 70, '英語' => 90, '国語' => 80);
+
+    // この下にforeach文を書いてください
+    foreach($scores as $key => $value){
+      echo $key."は".$value."点です。";
+    }
+    
+  ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    $str = 'progate';
+
+    // strlenを用いて$strの長さをechoしてください
+    echo strlen($str);
+    
+    
+    echo '<br>';
+    
+    $array = array('HTML', 'CSS', 'PHP');
+
+    // countを用いて$arrayの要素数をechoしてください
+    
+    echo count($array);
+    echo '<br>';
+    
+    // randを用いて10から15までのランダムな数字をechoしてください
+    echo rand(10,15);
+    
+  ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 関数helloを定義してください
+    function hello(){
+      echo "Hello, world!";
+    }
+    
+    // 関数helloを呼び出してください
+    hello();
+    echo '<br>';
+    // 関数printRectangleAreaを定義してください
+    
+    function printRectangleArea($height,$width){
+      echo $height*$width;
+    }
+    
+    // 引数を(5, 10)としてprintRectangleAreaを呼び出してください
+    printRectangleArea(5,10);
+    
+  ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 関数getCircleAreaを定義してください
+    
+    function getCircleArea($radius){
+      return $radius * $radius * 3;
+    }
+    
+    // 関数getCircleAreaを呼び出して、戻り値を変数$circleAreaに代入してください
+    $circleArea = getCircleArea(5);
+    
+    
+    // $circleAreaをechoしてください
+    echo $circleArea;
+    
+  ?>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <div class="header">
+    <div class="header-left">Progate</div>
+    <div class="header-right">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li class="selected">お問い合わせ</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="main">
+    <div class="contact-form">
+      <div class="form-title">お問い合わせ</div>
+      <!-- この下にformタグを書いていきましょう -->
+      
+      <form action="sent.php" method="post">
+        
+      </form>
+      
+    </div>
+  </div>
+  
+  <div class="footer">
+    <div class="footer-left">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </div>
+    <div class="like-box">
+      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
+    </div>
+  </div>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <div class="header">
+    <div class="header-left">Progate</div>
+    <div class="header-right">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li class="selected">お問い合わせ</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="main">
+    <div class="contact-form">
+      <div class="form-title">お問い合わせ</div>
+      <form method="post" action="sent.php">
+        <div class="form-item">名前</div>
+        <input type="text" name="name">
+
+        <div class="form-item">内容</div>
+        <textarea name="body"></textarea>
+
+        <!-- この下に送信ボタンを作りましょう -->
+        <input type="submit" value="送信">
+        
+      </form>
+    </div>
+  </div>
+  
+  <div class="footer">
+    <div class="footer-left">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </div>
+    <div class="like-box">
+      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
+    </div>
+  </div>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <div class="header">
+    <div class="header-left">Progate</div>
+    <div class="header-right">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li class="selected">お問い合わせ</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="main">
+    <div class="contact-form">
+      <div class="form-title">お問い合わせ</div>
+      <form method="post" action="sent.php">
+        <div class="form-item">名前</div>
+        <input type="text" name="name">
+
+        <div class="form-item">年齢</div>
+        <!-- この下にselectタグを書いていきましょう -->
+        <select name="age">
+          <option value="未選択">選択してください</option>
+          <option value="20代">20代</option>
+          <option value="30代">30代</option>
+        </select>
+        
+        <div class="form-item">内容</div>
+        <textarea name="body"></textarea>
+
+        <input type="submit" value="送信">
+      </form>
+    </div>
+  </div>
+  
+  <div class="footer">
+    <div class="footer-left">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </div>
+    <div class="like-box">
+      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
+    </div>
+  </div>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <div class="header">
+    <div class="header-left">Progate</div>
+    <div class="header-right">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li class="selected">お問い合わせ</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="main">
+    <div class="contact-form">
+      <div class="form-title">お問い合わせ</div>
+      <form method="post" action="sent.php">
+        <div class="form-item">名前</div>
+        <input type="text" name="name">
+
+        <div class="form-item">年齢</div>
+        <select name="age">
+          <option value="未選択">選択してください</option>
+          <!-- for文を用いて6歳から100歳までをoptionで選べるようにしましょう -->
+          <?php 
+            for($i = 6;$i<=100;$i++){
+              echo "<option value='{$i}'>{$i}</option>";
+            }
+          ?>
+        </select>
+
+        <div class="form-item">お問い合わせの種類</div>
+        <?php 
+          $types = array('Progateに関するお問い合わせ', 'Progateに対する意見', '採用に関するお問い合わせ', '取材・メディア関連のお問い合わせ', '料金に関するお問い合わせ', 'その他');
+         ?>
+        <!-- この下にselectタグを書いていきましょう -->
+        
+        <select name="category">
+          <option value="未選択">選択してください</option>
+          <?php            
+             foreach ($types as $type) {            
+                  echo "<option value='{$type}'>{$type}</option>";            
+                   }            
+                 ?>
+        </select>
+        
+        <div class="form-item">内容</div>
+        <textarea name="body"></textarea>
+
+        <input type="submit" value="送信">
+      </form>
+    </div>
+  </div>
+  
+  <div class="footer">
+    <div class="footer-left">
+      <ul>
+        <li>会社概要</li>
+        <li>採用</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </div>
+    <div class="like-box">
+      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
+    </div>
+  </div>
+</body>
+</html>
