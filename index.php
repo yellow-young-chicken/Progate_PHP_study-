c2b8dd32cc8caebf359d2463e88c466ae82cf4d7
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-04 15:09:33 +0000

    3/4

--- index.php
+++ index.php
@@ -792,3 +792,19 @@ echo '<br>';
 echo 'My name is '.$name;
 
 ?>
+
+<?php
+$price = 1000;
+$taxRate = 0.08;
+echo '変数$priceの値: '.$price;
+echo '<br>';
+echo '変数$taxRateの値: '.$taxRate;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+
+$taxIncludedPrice = $price + $price * $taxRate;
+echo '税込価格は'.$taxIncludedPrice.'円です';
+?>
