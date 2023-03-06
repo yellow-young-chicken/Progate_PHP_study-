28f7d8c605d513ab3630e6159158c5fb9a1708f3
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-06 16:12:03 +0000

    3/6

--- index.php
+++ index.php
@@ -809,3 +809,28 @@ $taxIncludedPrice = $price + $price * $taxRate;
 echo '税込価格は'.$taxIncludedPrice.'円です';
 ?>
 
+<?php
+$money = 2000;
+$price = 1000;
+$taxRate = 0.08;
+echo '変数$moneyの値: '.$money;
+echo '<br>';
+echo '変数$priceの値: '.$price;
+echo '<br>';
+echo '変数$taxRateの値: '.$taxRate;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+$taxIncludedPrice = $price + $price * $taxRate;
+if ($money > $taxIncludedPrice) {
+  echo '商品を買うことができます';
+} elseif ($money == $taxIncludedPrice) {
+  echo '商品を買うことができますが、所持金がなくなります';
+} else {
+  echo '商品を買うことができません';
+}
+
+?>
+
