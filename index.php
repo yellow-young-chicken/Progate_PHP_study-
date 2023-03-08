b4f27cf91d88509088966c7e4a79223ada68242f
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-08 16:37:21 +0000

    3/8

--- index.php
+++ index.php
@@ -853,3 +853,29 @@ foreach ($prices as $price) {
 echo '合計金額は'.$totalPrice.'円です';
 
 ?>
+
+<?php
+$prices = array(1000, 650, 750, 800);
+echo '$pricesの値: ';
+foreach ($prices as $price) {
+  echo $price.' ';
+}
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+$totalPrice = 0;
+$maxPrice = 0; // 最高価格を入れるための変数
+foreach ($prices as $price) {
+  $totalPrice += $price;
+  // $maxPriceに入っている価格より大きければ
+  if ($price > $maxPrice) {
+    // その価格で$maxPriceを上書きする
+    $maxPrice = $price;
+  }
+}
+echo '合計金額は'.$totalPrice.'円です';
+echo '<br>';
+echo '最高価格は'.$maxPrice.'円です';
+?>
