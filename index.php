018d4b7f93ce4f7d5aa17d8ebd60fd6746fc589d
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-11 05:09:46 +0000

    3/11

--- index.php
+++ index.php
@@ -879,3 +879,17 @@ echo '合計金額は'.$totalPrice.'円です';
 echo '<br>';
 echo '最高価格は'.$maxPrice.'円です';
 ?>
+
+<?php
+$menu = array('name' => 'CURRY', 'price' => 900);
+echo '$menuの値: ';
+// var_exportは変数の中身を見るための関数です
+var_export($menu);
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+echo $menu['name'].'は'.$menu['price'].'円です';
+
+?>
