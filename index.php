718b57abf5303dc2cf306f1efa56ffb6921762fc
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-14 13:46:50 +0000

    3/14

--- index.php
+++ index.php
@@ -894,4 +894,18 @@ echo $menu['name'].'は'.$menu['price'].'円です';
 
 ?>
 
+<?php
+$menus = array(
+  array('name' => 'CURRY', 'price' => 900),
+  array('name' => 'PASTA', 'price' => 1200),
+  array('name' => 'COFFEE', 'price' => 600)
+);
+
+// この下にコードを書いてください
+foreach ($menus as $menu) {
+  echo $menu['name'].'は'.$menu['price'].'円です';
+  echo '<br>';
+}
+
+?>
 
