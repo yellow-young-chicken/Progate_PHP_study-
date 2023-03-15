c5021173523d8425f3551af768c5b0c896320f05
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-15 15:26:32 +0000

    3/15

--- index.php
+++ index.php
@@ -909,3 +909,51 @@ foreach ($menus as $menu) {
 
 ?>
 
+<?php
+$menus = array(
+  array('name' => 'CURRY', 'price' => 900),
+  array('name' => 'PASTA', 'price' => 1200),
+  array('name' => 'COFFEE', 'price' => 600)
+);
+
+$totalPrice = 0; // 合計金額を入れるための変数
+foreach ($menus as $menu) {
+  $price = $menu['price'];
+  echo $menu['name'].'は'.$price.'円です';
+  echo '<br>';
+  // それぞれの価格を$totalPriceに足していく
+  $totalPrice += $price;
+}
+echo '合計金額は'.$totalPrice.'円です';
+
+?>
+
+<?php
+$menus = array(
+  array('name' => 'CURRY', 'price' => 900),
+  array('name' => 'PASTA', 'price' => 1200),
+  array('name' => 'COFFEE', 'price' => 600)
+);
+
+$totalPrice = 0;
+$maxPrice = 0; // 最高価格を入れるための変数
+$maxPriceMenuName = ''; // 最高価格のものの名前を入れるための変数
+foreach ($menus as $menu) {
+  $name = $menu['name'];
+  $price = $menu['price'];
+  echo $name.'は'.$price.'円です';
+  echo '<br>';
+  $totalPrice += $price;
+  // $maxPriceに入っている価格より大きければ
+  if ($price > $maxPrice) {
+    // その価格で$maxPriceを上書きする
+    $maxPrice = $price;
+    // 最高価格のものの名前で上書きする
+    $maxPriceMenuName = $name;
+  }
+}
+echo '合計金額は'.$totalPrice.'円です';
+echo '<br>';
+echo $maxPriceMenuName.'が最高価格で'.$maxPrice.'円です';
+
+?>
