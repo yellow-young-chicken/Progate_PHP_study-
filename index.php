880c9025048e98759a442f0dbbb0a050a3ed3fd3
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-21 16:49:31 +0000

    3/21

--- index.php
+++ index.php
@@ -1019,3 +1019,49 @@ $pasta->hello();
 
 ?>
 
+<?php
+class Menu {
+  public $name;
+  
+  public function hello() {
+    // '私は○○です'とechoしてください
+    echo '私は'.$this->name.'です';
+    
+  }
+}
+
+$curry = new Menu();
+$pasta = new Menu();
+$curry->name = 'CURRY';
+$pasta->name = 'PASTA';
+$curry->hello();
+echo '<br>';
+$pasta->hello();
+
+?>
+
+<?php
+class Menu {
+  public $name;
+  
+  // コンストラクタを定義してください
+  public function __construct() {
+    echo "1つのメニューが作られました";
+  }
+  
+  public function hello() {
+    echo '私は'.$this->name.'です';
+  }
+}
+
+$curry = new Menu();
+echo '<br>';
+$pasta = new Menu();
+echo '<br>';
+$curry->name = 'CURRY';
+$pasta->name = 'PASTA';
+$curry->hello();
+echo '<br>';
+$pasta->hello();
+
+?>
