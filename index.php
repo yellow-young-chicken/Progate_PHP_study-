3ef51c0bcc40ee5b3965d9f39880e6e163a55bd7
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-24 16:47:29 +0000

    3/24

--- index.php
+++ index.php
@@ -1095,3 +1095,72 @@ echo '<br>';
 $pasta->hello();
 
 ?>
+
+
+<?php
+class Menu {
+  public $name;
+  
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  
+  public function hello() {
+    echo '私は'.$this->name.'です';
+  }
+}
+
+$curry = new Menu('CURRY');
+$pasta = new Menu('PASTA');
+
+?>
+
+<!-- <p>タグの中で、$curryのnameプロパティを表示してください -->
+<p><?php echo $curry->name ?></p>
+
+<!-- <p>タグの中で、$pastaのnameプロパティを表示してください -->
+<p><?php echo $pasta->name ?></p>
+
+<?php
+class Menu {
+  public $name;
+  
+  public function __construct($name) {
+    $this->name = $name;
+  }
+  
+  public function hello() {
+    echo '私は'.$this->name.'です';
+  }
+}
+
+$juice = new Menu('JUICE');
+$coffee = new Menu('COFFEE');
+$curry = new Menu('CURRY');
+$pasta = new Menu('PASTA');
+// 配列の中に上記の4つのインスタンスを順に入れて、変数$menusに代入してください
+$menus = array($juice, $coffee, $curry, $pasta);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Progate</h1>
+    <div class="menu-items">
+      <!-- 配列$menusの要素を変数$menuとするforeach文を書いてください -->
+      <?php foreach ($menus as $menu): ?>
+        <h3><?php echo $menu->name ?></h3>
+      <?php endforeach ?>
+      
+    </div>
+  </div>
+</body>
+</html>
