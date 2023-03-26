584326b706ca55fa41043e091c2bc79b51503b7e
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-26 19:21:28 +0000

    3/26

--- index.php
+++ index.php
@@ -1164,3 +1164,39 @@ $menus = array($juice, $coffee, $curry, $pasta);
   </div>
 </body>
 </html>
+
+
+<?php
+// クラスの定義部分をmenu.phpに移してください
+
+// ここまでクラスの宣言部分
+
+// 以下のデータ定義部分をdata.phpに移してください
+
+// ここまでデータ定義部分
+
+?>
+
+<!-- ここでdata.phpを読み込んでください  -->
+<?php require_once('data.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Progate</h1>
+    <div class="menu-items">
+      <?php foreach ($menus as $menu): ?>
+        <h3 class="menu-item-name"><?php echo $menu->name ?></h3>
+      <?php endforeach ?>
+      
+    </div>
+  </div>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,11 @@
+<?php
+// クラスの宣言部分をここに移してください
+class Menu {
+  public $name;
+  
+  public function __construct($name) {
+    $this->name = $name;
+  }
+}
+
+?>
