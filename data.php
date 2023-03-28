ae51ae5c53a686f1924437454978830bce70bf47
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-28 18:10:46 +0000

    3/28

--- data.php
+++ data.php
@@ -11,3 +11,16 @@ $pasta = new Menu('PASTA');
 $menus = array($juice, $coffee, $curry, $pasta);
 
 ?>
+
+<?php
+require_once('menu.php');
+
+// それぞれpriceとimageを追加してください
+$juice = new Menu('JUICE', 600, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png');
+$coffee = new Menu('COFFEE', 500, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png');
+$curry = new Menu('CURRY', 900, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png');
+$pasta = new Menu('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png');
+
+$menus = array($juice, $coffee, $curry, $pasta);
+
+?>
