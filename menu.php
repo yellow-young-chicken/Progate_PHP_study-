1cb3fe85004c259177fc13fe3e4d99e6d3a99c9e
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-29 17:36:28 +0000

    3/29

--- menu.php
+++ menu.php
@@ -28,3 +28,23 @@ class Menu {
   }
 }
 ?>
+
+<?php
+class Menu {
+  public $name;
+  public $price;
+  public $image;
+  
+  public function __construct($name, $price, $image) {
+    $this->name = $name;
+    $this->price = $price;
+    $this->image = $image;
+  }
+  
+  // getTaxIncludedPriceメソッドを定義してください
+  public function getTaxIncludedPrice() {
+    return floor($this->price * 1.08);
+  }
+  
+}
+?>
