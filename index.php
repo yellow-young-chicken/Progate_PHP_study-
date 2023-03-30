53a28ecbd2af59ce4a2d7958fd6c234754483aae
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-30 16:48:51 +0000

    3/30

--- index.php
+++ index.php
@@ -1255,3 +1255,32 @@ $menus = array($juice, $coffee, $curry, $pasta);
   </div>
 </body>
 </html>
+
+
+<?php require_once('data.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Café Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <div class="menu-wrapper container">
+    <h1 class="logo">Café Progate</h1>
+    <div class="menu-items">
+      <?php foreach ($menus as $menu): ?>
+        <div class="menu-item">
+          <!-- imageプロパティのゲッターを用いて以下を書き換えてください -->
+          <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
+          <!-- nameプロパティのゲッターを用いて以下を書き換えてください -->
+          <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
+          <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?>（税込）</p>
+        </div>
+      <?php endforeach ?>
+    </div>
+  </div>
+</body>
+</html>
--- menu.php
+++ menu.php
@@ -48,3 +48,33 @@ class Menu {
   
 }
 ?>
+
+<?php
+class Menu {
+  // name, price, imageプロパティのアクセス権をprivateにしてください
+  private $name;
+  private $price;
+  private $image;
+  
+  public function __construct($name, $price, $image) {
+    $this->name = $name;
+    $this->price = $price;
+    $this->image = $image;
+  }
+  
+  // getNameメソッドを定義してください
+  public function getName() {
+    return $this->name;
+  }
+  
+  // getImageメソッドを定義してください
+  public function getImage() {
+    return $this->image;
+  }
+  
+  public function getTaxIncludedPrice() {
+    return floor($this->price * 1.08);
+  }
+  
+}
+?>
