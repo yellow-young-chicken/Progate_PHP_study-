ec56069a50b25c0e107baf1d6c4f833bf7308473
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-03-31 16:37:55 +0000

    3/31

--- data.php
+++ data.php
@@ -24,3 +24,18 @@ $pasta = new Menu('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progat
 $menus = array($juice, $coffee, $curry, $pasta);
 
 ?>
+
+<?php
+require_once('menu.php');
+
+$juice = new Menu('JUICE', 600, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png');
+$coffee = new Menu('COFFEE', 500, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png');
+$curry = new Menu('CURRY', 900, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png');
+$pasta = new Menu('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png');
+
+// $juiceに対して数値の2を引数としてsetOrderCountメソッドを呼び出してください
+$juice->setOrderCount(2);
+
+$menus = array($juice, $coffee, $curry, $pasta);
+
+?>
--- menu.php
+++ menu.php
@@ -78,3 +78,42 @@ class Menu {
   
 }
 ?>
+
+<?php
+class Menu {
+  private $name;
+  private $price;
+  private $image;
+  // $orderCountというプロパティを定義してください。ただし、初期値を数値の0としてください。
+  private $orderCount = 0;
+  
+  public function __construct($name, $price, $image) {
+    $this->name = $name;
+    $this->price = $price;
+    $this->image = $image;
+  }
+  
+  public function getName() {
+    return $this->name;
+  }
+  
+  public function getImage() {
+    return $this->image;
+  }
+  
+  // getOrderCountメソッドを定義してください
+  public function getOrderCount() {
+    return $this->orderCount;
+  }
+  
+  // setOrderCountメソッドを定義してください
+  public function setOrderCount($orderCount) {
+    $this->orderCount = $orderCount;
+  }
+  
+  public function getTaxIncludedPrice() {
+    return floor($this->price * 1.08);
+  }
+  
+}
+?>
