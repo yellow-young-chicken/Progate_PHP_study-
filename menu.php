b22352da2d28b8f011da68cf930e1021ffcaca67
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-04-06 11:10:34 +0000

    4/6

--- menu.php
+++ menu.php
@@ -117,3 +117,44 @@ class Menu {
   
 }
 ?>
+
+<?php
+class Menu {
+  private $name;
+  private $price;
+  private $image;
+  private $orderCount = 0;
+  
+  public function __construct($name, $price, $image) {
+    $this->name = $name;
+    $this->price = $price;
+    $this->image = $image;
+  }
+  
+  public function getName() {
+    return $this->name;
+  }
+  
+  public function getImage() {
+    return $this->image;
+  }
+  
+  public function getOrderCount() {
+    return $this->orderCount;
+  }
+  
+  public function setOrderCount($orderCount) {
+    $this->orderCount = $orderCount;
+  }
+  
+  public function getTaxIncludedPrice() {
+    return floor($this->price * 1.08);
+  }
+  
+  // getTotalPriceメソッドを定義してください
+  public function getTotalPrice() {
+    return $this->getTaxIncludedPrice() * $this->orderCount;
+  }
+  
+}
+?>
