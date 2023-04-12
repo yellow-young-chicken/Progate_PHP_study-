cbf92387b7b8237904af83b11057bb3ac6951adf
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-04-12 17:09:50 +0000

    4/12

--- data.php
+++ data.php
@@ -59,3 +59,20 @@ $pasta = new Food('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progat
 $menus = array($juice, $coffee, $curry, $pasta);
 
 ?>
+
+<?php
+require_once('drink.php');
+require_once('food.php');
+
+$juice = new Drink('JUICE', 600, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png');
+$coffee = new Drink('COFFEE', 500, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png');
+
+// $juiceのセッターを用いて$typeプロパティに'アイス'を代入してください
+$juice->setType('アイス');
+
+// $coffeeのセッターを用いて$typeプロパティに'ホット'を代入してください
+$coffee->setType('ホット');
+
+$menus = array($juice, $coffee);
+
+?>
--- drink.php
+++ drink.php
@@ -8,3 +8,24 @@ class Drink extends Menu {
 }
 
 ?>
+
+<?php 
+require_once('menu.php');
+
+class Drink extends Menu {
+  // $typeというprivateなプロパティを定義してください
+  private $type;
+  
+  // getTypeメソッドを定義してください
+  public function getType() {
+    return $this->type;
+  }
+  
+  // setTypeメソッドを定義してください
+  public function setType($type) {
+    $this->type = $type;
+  }
+  
+}
+
+?>
