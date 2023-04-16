e398d9c83b8173321fca13e15bc84c7ae8db6c0a
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-04-16 17:31:23 +0000

    4/16

--- data.php
+++ data.php
@@ -76,3 +76,21 @@ $coffee->setType('ホット');
 $menus = array($juice, $coffee);
 
 ?>
+
+<?php
+require_once('drink.php');
+require_once('food.php');
+
+// $juiceのnewの引数に'アイス'を追加してください
+$juice = new Drink('JUICE', 600, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png', 'アイス');
+// $coffeeのnewの引数に'ホット'を追加してください
+$coffee = new Drink('COFFEE', 500, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png', 'ホット');
+$curry = new Food('CURRY', 900, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png');
+$pasta = new Food('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png');
+
+// 以下の2行を削除してください
+
+
+$menus = array($juice, $coffee, $curry, $pasta);
+
+?>
