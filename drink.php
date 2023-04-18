bd556a9806d3e0ed5e6bd6774e8e764e98ebf137
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-04-18 16:38:07 +0000

    4/18

--- drink.php
+++ drink.php
@@ -60,3 +60,30 @@ class Drink extends Menu {
 }
 
 ?>
+
+<?php 
+require_once('menu.php');
+
+class Drink extends Menu {
+  private $type;
+  
+  public function __construct($name, $price, $image, $type) {
+    // 以下の4行を削除してください
+    
+    // 親クラスのコンストラクタを呼び出してください
+    parent::__construct($name, $price, $image);
+    
+    $this->type = $type;
+  }
+  
+  public function getType() {
+    return $this->type;
+  }
+  
+  public function setType($type) {
+    $this->type = $type;
+  }
+  
+}
+
+?>
