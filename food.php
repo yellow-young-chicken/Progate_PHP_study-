7ab974b16c5bd6960aa2e63674cd4eac88fb3ac5
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-04-20 18:00:48 +0000

    4/20

--- food.php
+++ food.php
@@ -9,3 +9,24 @@ class Food extends Menu {
 
 ?>
 
+<?php 
+require_once('menu.php');
+
+class Food extends Menu {
+  // $spicinessというprivateなプロパティを定義してください
+  private $spiciness;
+  
+  // 親クラスの__constructをオーバーライドしてください
+  public function __construct($name, $price, $image, $spiciness) {
+    parent::__construct($name, $price, $image);
+    $this->spiciness = $spiciness;
+  }
+  
+  // getSpicinessメソッドを定義してください
+  public function getSpiciness() {
+    return $this->spiciness;
+  }
+  
+}
+
+?>
--- index.php
+++ index.php
@@ -1462,3 +1462,25 @@ require_once('menu.php');
   </div>
 </body>
 </html>
+
+<?php 
+require_once('menu.php');
+
+class Food extends Menu {
+  // $spicinessというprivateなプロパティを定義してください
+  private $spiciness;
+  
+  // 親クラスの__constructをオーバーライドしてください
+  public function __construct($name, $price, $image, $spiciness) {
+    parent::__construct($name, $price, $image);
+    $this->spiciness = $spiciness;
+  }
+  
+  // getSpicinessメソッドを定義してください
+  public function getSpiciness() {
+    return $this->spiciness;
+  }
+  
+}
+
+?>
